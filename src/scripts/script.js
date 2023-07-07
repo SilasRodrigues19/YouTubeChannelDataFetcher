85db08bad8a6882e56cda493ff04d961fa440807
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-07-06 21:02:43 -0300

    feat: build iframe with video data brought from API

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,5 @@
 const API_KEY = import.meta.env.PUBLIC_YOUTUBE_API_KEY;
+let player;
 
 document.addEventListener('DOMContentLoaded', () => {
   const inputQueryElement = document.querySelector('#inputQuery');
@@ -6,10 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const videoDataElement = document.querySelector('#videoData');
   const titleElement = document.querySelector('#title');
   const descriptionElement = document.querySelector('#description');
+  const playerContainerElement = document.querySelector('#playerContainer');
 
   submitButtonElement.addEventListener('click', () => {
     const query = inputQueryElement.value;
-    if(!!query) fetchVideoData(query);
+    if (!!query) fetchVideoData(query);
   });
 
   const fetchVideoData = (query) => {
@@ -27,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
           const linkRegex = /https?:\/\/[^\s]+/g;
           if (linkRegex.test(description)) {
-            description = description.replace(linkRegex, match => {
+            description = description.replace(linkRegex, (match) => {
               return `<a class="text-indigo-500 hover:underline" href="${match}" target="_blank" rel="noopener noreferrer">${match}</a>`;
             });
           }
@@ -35,6 +37,9 @@ document.addEventListener('DOMContentLoaded', () => {
           titleElement.textContent = title;
           descriptionElement.innerHTML = description;
           videoDataElement.style.display = 'block';
+
+          const videoId = items[0].id.videoId;
+          createYouTubePlayer(videoId);
         } else {
           titleElement.textContent = 'Nenhum resultado';
           descriptionElement.textContent = '';
@@ -45,4 +50,8 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error('Ocorreu um erro:', error);
       });
   };
+
+  function createYouTubePlayer(videoId) {
+    playerContainerElement.innerHTML = `<iframe width="400" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
+  }
 });
